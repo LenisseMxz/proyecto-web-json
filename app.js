396129139c375e25aa8e76ebe920e2192ad692ed
llopez95@gmail.com <LenisseMxz>
2026-03-10 17:43:48 -0600

    V3

---
+++ app.js
@@ -0,0 +1,59 @@
+function mostrarComentarios(postId) {
+    let divCommentsUsuario = document.getElementById(`comment${postId}`);
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+        .then(res => res.json())
+        .then(comments => {
+            comments.forEach(comment => {
+                divCommentsUsuario.innerHTML += `<div class="card">
+                                                    <div class="card-header">${comment.email}</div>
+                                                    <div class="card-body">
+                                                        <h6 class="card-title">${comment.name}</h6>
+                                                        <p class="card-text">${comment.body}</p>
+                                                    </div>
+                                                </div>`
+            })
+        })
+}
+
+function ocultarComentarios(postId) {
+    let divCommentsUsuario = document.getElementById(`comment${postId}`);
+    fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
+        .then(res => res.json())
+        .then(comments => {
+            divCommentsUsuario.innerHTML = "";
+        })
+}
+
+window.mostrarComentarios = mostrarComentarios;
+window.ocultarComentarios = ocultarComentarios;
+
+const btnCargar = document.getElementById("btnCargar");
+const slcUsuarios = document.getElementById("slc-usuarios");
+const divPostsUsuarios = document.getElementById("div-posts-usuarios");
+btnCargar.addEventListener("click", () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(usuarios => {
+            usuarios.forEach(usuario => {
+                slcUsuarios.innerHTML += `<option id="${usuario.id}" value="${usuario.id}">${usuario.name}</option>`;
+            })
+        })
+})
+
+slcUsuarios.addEventListener("change", () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts?userId=${slcUsuarios.value}`)
+        .then(res => res.json())
+        .then(posts => {
+            posts.forEach(post => {
+                divPostsUsuarios.innerHTML += `<div class="card text-bg-light mb-3" style="max-width: 18rem;">
+                                                        <div class="card-body">
+                                                            <h5 class="card-title">${post.title}</h5>
+                                                            <p class="card-text">${post.body}</p>
+                                                            <button type="button" class="btn btn-info" onclick="mostrarComentarios(${post.id})">Ver comentarios</button>
+                                                            <button type="button" class="btn btn-info" onclick="ocultarComentarios(${post.id})">Ocultar comentarios</button>
+                                                            <div id="comment${post.id}"></div>
+                                                        </div>
+                                                </div>`;
+            })
+        })
+})
