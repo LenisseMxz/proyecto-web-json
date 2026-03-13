8ca2d9a786b7961e561560c07150981dcbec2b47
llopez95@gmail.com <LenisseMxz>
2026-03-12 22:33:05 -0600

    V4

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function mostrarComentarios(postId) {
     let divCommentsUsuario = document.getElementById(`comment${postId}`);
+    const misComentarios = JSON.parse(localStorage.getItem("misComentarios") || []);
+
     fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
         .then(res => res.json())
         .then(comments => {
@@ -10,8 +12,17 @@ function mostrarComentarios(postId) {
                                                         <h6 class="card-title">${comment.name}</h6>
                                                         <p class="card-text">${comment.body}</p>
                                                     </div>
-                                                </div>`
+                                                </div>`;
             })
+            if (misComentarios.postId == postId){
+                divCommentsUsuario.innerHTML += `<div class="card">
+                                                    <div class="card-header">${misComentarios.email}</div>
+                                                    <div class="card-body">
+                                                        <h6 class="card-title">${misComentarios.name}</h6>
+                                                        <p class="card-text">${misComentarios.body}</p>
+                                                    </div>
+                                                </div>`;
+            }
         })
 }
 
@@ -24,8 +35,62 @@ function ocultarComentarios(postId) {
         })
 }
 
+function agregarComentario(postId) {
+    let divCommentFormulario = document.getElementById(`form${postId}`);
+    let divCommentsUsuario = document.getElementById(`comment${postId}`);
+
+    divCommentFormulario.innerHTML = `<form id="form-comment">
+                                            <div class="mb-3">
+                                                <label for="input-name" class="form-label">Nombre</label>
+                                                <input type="text" class="form-control" id="input-name">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label for="input-email" class="form-label">Email</label>
+                                                <input type="email" class="form-control" id="input-email">
+                                            </div>
+                                            <div class="mb-3">
+                                                <label class="input-body" for="exampleCheck1">Cuerpo</label>
+                                                <input type="text" class="form-control" id="input-body">
+                                            </div>
+                                            <button type="submit" class="btn btn-primary" id="submit">Enviar</button>
+                                        </form>`;
+
+    let formCommment = document.getElementById("form-comment");
+
+    formCommment.addEventListener("submit", () => {
+
+        event.preventDefault();
+
+        let name = document.getElementById("input-name").value;
+        let email = document.getElementById("input-email").value;
+        let body = document.getElementById("input-body").value;
+
+        let misComentarios = 
+
+        fetch(`https://jsonplaceholder.typicode.com/comments`, {
+            method: 'POST',
+            body: JSON.stringify({
+                postId: `${postId}`,
+                name: `${name}`,
+                email: `${email}`,
+                body: `${body}`
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            localStorage.setItem("misComentarios", JSON.stringify(data) || []);
+        })
+        divCommentFormulario.innerHTML = "";
+    })
+}
+
 window.mostrarComentarios = mostrarComentarios;
 window.ocultarComentarios = ocultarComentarios;
+window.agregarComentario = agregarComentario;
 
 const btnCargar = document.getElementById("btnCargar");
 const slcUsuarios = document.getElementById("slc-usuarios");
@@ -51,6 +116,8 @@ slcUsuarios.addEventListener("change", () => {
                                                             <p class="card-text">${post.body}</p>
                                                             <button type="button" class="btn btn-info" onclick="mostrarComentarios(${post.id})">Ver comentarios</button>
                                                             <button type="button" class="btn btn-info" onclick="ocultarComentarios(${post.id})">Ocultar comentarios</button>
+                                                            <button type="button" class="btn btn-info" onclick="agregarComentario(${post.id})">Agregar comentario</button>
+                                                            <div id="form${post.id}"></div>
                                                             <div id="comment${post.id}"></div>
                                                         </div>
                                                 </div>`;
